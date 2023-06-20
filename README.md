4f4679df87ac3ec37917c7e99d8a11ea55d77dd8
hamed_p <hamed_p@gmail.com>
2023-06-20 17:54:50 +0330

    add comment

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
+
 # Train_QA_huggingface
-This code is a machine learning pipeline that trains an AI model to perform question-answering (QA) tasks using the transformers library.
+ 
+This code trains and evaluates a Question Answering (QA) model using the transformers library on a Persian dataset.
+
+
+ 
+## Requirements 
+ 
+- pandas
+- transformers['torch]
+- datasets
+
+## Installation 
+ 
+Make sure to install all the required libraries mentioned in the code. 
+ 
+## How to use 
+ 
+To use the code, just set the parameters as required.
+after that code will doing this steps:
+
+- Read the train and test data using the read_qa() function provided in utils.read_ds module.
+- Create a dataset dictionary and convert the data to datasets.
+- Preprocess the data and tokenize.
+- Load the model weights using AutoModelForQuestionAnswering from transformers library.
+- Set the training arguments using TrainingArguments from transformers library.
+- Train the model using Trainer from transformers library.
+- Evaluate the model.
+The code has been written in a way that the parameters and the functions used for data preprocessing and tokenization can be customized.
--- main.py
+++ main.py
@@ -1,39 +1,56 @@
-from datasets import DatasetDict
 from utils.read_ds import read_qa
-from transformers import pipeline
 from utils.preprocess import preprocess_function
-from transformers import DefaultDataCollator, AutoTokenizer
-from transformers import AutoModelForQuestionAnswering,TrainingArguments,Trainer
-from datasets import load_dataset
-from datasets import Dataset
 import pandas as pd
+from transformers import AutoTokenizer, DefaultDataCollator, AutoModelForQuestionAnswering, TrainingArguments, Trainer
+from datasets import Dataset, DatasetDict
 
+# Set parameters
+params = {
+    "train_data_path": "./data/pqa_train.json",
+    "test_data_path": "./data/pqa_test.json",
+    "model_dir_path": "./albert-fa-base-v2_pquad_and_persian_qa",
+    "output_dir_path": "trained_qa_model",
+    "evaluation_strategy": "epoch",
+    "learning_rate": 2e-5,
+    "train_batch_size": 8,
+    "eval_batch_size": 8,
+    "num_train_epochs": 1,
+    "weight_decay": 0.01,
+    "push_to_hub": False,
+    "num_train_samples": 100,
+    "num_test_samples": 20
+}
 
-a=read_qa('./data/pqa_train.json')
+# Read the data
+train_data = read_qa(params["train_data_path"])
+test_data = read_qa(params["test_data_path"])
+
+# Create a dataset dictionary and convert the data to datasets
 dataset = DatasetDict()
-dataset["train"] = Dataset.from_pandas(pd.DataFrame(data=read_qa('./data/pqa_train.json')).dropna()[:100])
-dataset["test"]  = Dataset.from_pandas(pd.DataFrame(data=read_qa('./data/pqa_test.json')).dropna()[:20])
-# dataset = load_dataset("yelp_review_full")
-tokenizer = AutoTokenizer.from_pretrained("./albert-fa-base-v2_pquad_and_persian_qa")
+dataset["train"] = Dataset.from_pandas(pd.DataFrame(data=train_data).dropna()[:params["num_train_samples"]])
+dataset["test"]  = Dataset.from_pandas(pd.DataFrame(data=test_data).dropna()[:params["num_test_samples"]])
 
+# Preprocess the data and tokenize
+tokenizer = AutoTokenizer.from_pretrained(params["model_dir_path"])
 tokenized_map_data = dataset.map(preprocess_function, batched=True)
-
 data_collator = DefaultDataCollator()
 
-model = AutoModelForQuestionAnswering.from_pretrained("./albert-fa-base-v2_pquad_and_persian_qa")
+# Load the model weights
+model = AutoModelForQuestionAnswering.from_pretrained(params["model_dir_path"])
 
+# Set the training arguments
 training_args = TrainingArguments(
-     output_dir="traned_qa_model",
-     evaluation_strategy="epoch",
-     learning_rate=2e-5,
-     per_device_train_batch_size=8,
-     per_device_eval_batch_size=8,
-     num_train_epochs=1,
-     weight_decay=0.01,
-     push_to_hub=False,
- 
+    output_dir=params["output_dir_path"],
+    evaluation_strategy=params["evaluation_strategy"],
+    learning_rate=params["learning_rate"],
+    per_device_train_batch_size=params["train_batch_size"],
+    per_device_eval_batch_size=params["eval_batch_size"],
+    num_train_epochs=params["num_train_epochs"],
+    weight_decay=params["weight_decay"],
+    push_to_hub=params["push_to_hub"]
 )
 
+# Train the model
 trainer = Trainer(
      model=model,
      args=training_args,
@@ -41,8 +58,9 @@ trainer = Trainer(
      eval_dataset=tokenized_map_data["test"],
      tokenizer=tokenizer,
      data_collator=data_collator,
- )
-
+)
 trainer.train()
+
+# Evaluate the model
 result = trainer.evaluate()  
 print(result)
